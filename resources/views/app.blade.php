ce1216c4d6875233e56a0d667a8581e78d20813e
Brendon Tiszka <bjt2p3@mail.missouri.edu>
2015-04-05 20:19:57 -0700

    First design iteration; meh

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,15 +7,45 @@
 	<title>Laravel</title>
 
 	<link href="/css/app.css" rel="stylesheet">
-	@yield('headers');
+	@yield('headers')
+	<link href="/css/overrides.css" rel="stylesheet">
 
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+	<link href='//fonts.googleapis.com/css?family=Raleway:100' rel='stylesheet' type='text/css'>
 
 </head>
 <body>
-
-	@yield('content')
-
+	<nav class="navbar navbar-default" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+					<a class="logo" href="/">
+						Mizzou TA Application
+					</a>
+			</div>
+			<div class="collapse navbar-collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li>
+						<a target="_blank" href="">Insructions</a>
+					</li>
+					<li>
+						<a target="_blank" href="">Reference</a>
+					</li>
+					<li>
+						<a target="_blank" href="">Help</a>
+					</li>
+					<li>
+						<a href="#TODO">Sign In</a>
+					</li>
+					<li>
+						<a href="#TODO">Register</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<div class="container">
+		@yield('content')
+	</div>
 	<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,9 +1,41 @@
 @extends('app')
 
 @section('headers')
-<link href="/css/form.css" rel="stylesheet">
+<link href="/css/form.css" rel="stylesheet" />
 @endsection
 
 @section('content')
-<h1>TODO!</h1>
+<form class="form-inline">
+  <div class="form-group">
+    <div class="col-md-3">
+      <label for="studentName">Full Name</label>
+      <input type="text" class="form-control" id="studentName" placeholder="Full Name">
+    </div>
+
+    <div class="col-md-3">
+      <label for="studentPhone">Phone Number</label>
+      <input type="text" class="form-control" id="studentName" placeholder="Full Name">
+    </div>
+
+    <div class="col-md-3">
+      <label for="studentId">Student Id</label>
+      <input type="text" class="form-control" id="studentId" placeholder="Student Id">
+    </div>
+
+    <div class="col-md-3">
+      <label for="studentGPA">Grade Point Average</label>
+      <input type="text" class="form-control" id="studentGPA" placeholder="3.14">
+    </div>
+  </div>
+
+  <div class="form-group">
+    <div class="col-md-3">
+      <label for="studentEmail">Email</label>
+      <input type="text" class="form-control" id="studentEmail" placeholder="Student Id">
+    </div>
+  </div>
+</form>
+<div class="submit-button">
+  <button type="submit" class="btn btn-primary">Submit Application</button>
+</div>
 @endsection
