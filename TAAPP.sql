9a6b2d2e76962e35feefd2419a496d44847d96cd
Tim Laidlaw <laidlawt@missouri.edu>
2015-04-12 15:01:54 -0500

    Broke out DDL & Seed Data, Added Seed Data

--- TAAPP.sql
+++ TAAPP.sql
@@ -5,7 +5,8 @@ CREATE SCHEMA TAAPP;
 
 SET search_path = TAAPP;
 
-CREATE TABLE Applicant--Create Table for Applicant with all pertinent info
+
+CREATE TABLE Applicant
 (
  SSO VARCHAR(20) NOT NULL DEFAULT '',
  LastName VARCHAR(20) NOT NULL,
@@ -13,12 +14,13 @@ CREATE TABLE Applicant--Create Table for Applicant with all pertinent info
  Phone INTEGER NOT NULL,
  Email VARCHAR(40) NOT NULL,
  GPA VARCHAR(4) NOT NULL,
+ Graduate BOOLEAN NOT NULL,
  GradDate DATE NOT NULL,
  SpeakScore INTEGER,
  PRIMARY KEY(SSO)
 );
 
-CREATE TABLE Instructor--Create Table for Instructor(s) of Course(s)
+CREATE TABLE Instructor
 (
  SSO VARCHAR(20) NOT NULL DEFAULT '',
  LastName VARCHAR(20) NOT NULL,
@@ -26,17 +28,17 @@ CREATE TABLE Instructor--Create Table for Instructor(s) of Course(s)
  PRIMARY KEY(SSO)
 );
 
-CREATE TABLE ApplicantCourse--Create Table for an Applicant to a Course with a given Rank
+CREATE TABLE ApplicantCourse
 (
  SSO VARCHAR(20) NOT NULL DEFAULT '',
  CourseID VARCHAR(10) NOT NULL,
  Rank INTEGER NOT NULL,
- AcceptStatus BOOLEAN NOT NULL,
+ AcceptStatus BOOLEAN NOT NULL DEFAULT '0',
  SectionID VARCHAR(1),
  PRIMARY KEY(SSO, CourseID)
 );
 
-CREATE TABLE Course--Create Table for Course(s) taught by an Instructor
+CREATE TABLE Course
 (
  CourseID VARCHAR(10) NOT NULL,
  CourseName VARCHAR(40) NOT NULL,
@@ -44,7 +46,7 @@ CREATE TABLE Course--Create Table for Course(s) taught by an Instructor
  PRIMARY KEY(CourseID)
 );
 
-CREATE TABLE Section--Create Table for at least one Section within a Course, assigned to a TA
+CREATE TABLE Section
 (
  SectionID VARCHAR(1) NOT NULL,
  SectionName VARCHAR(20) NOT NULL,
@@ -53,19 +55,3 @@ CREATE TABLE Section--Create Table for at least one Section within a Course, ass
  DateTime DATE NOT NULL,
  PRIMARY KEY(SectionID)
 );
-
---INSERT INTO Applicant VALUES ('abc123','Smith','John','5735551234','fake@fake.fake','3.59','12/2015','100');
---INSERT INTO Applicant VALUES ('zyx987','Nixon','Richard','888555431','jowels@president.fake','2.10','05/50','60');
-
---INSERT INTO Instructor VALUES ('qwertyj','Qwerty','Jack');
-
---INSERT INTO ApplicantCourse VALUES ('abc123','CS4320','1','0','A');
---INSERT INTO ApplicantCourse VALUES ('zyx987','CS4320','2','0','A');
-
---INSERT INTO Course VALUES ('CS4320','Software Engineering','qwertyj');
-
---INSERT INTO Section VALUES ('A','Monday Morning','abc123','CS4320','Mon 8AM');
---INSERT INTO Section VALUES ('A','Monday Morning','zyx987','CS4320','Mon 8AM');
-
-
-
