c1bb868af17b2c1f86a5c6cbb3cad157a6a1daf7
lawgorithm <lawrencewilliams.lw@gmail.com>
2015-04-19 12:28:43 -0500

    added migration for applicant table

---
+++ database/migrations/2015_04_18_210017_create_applicant_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApplicantTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('Applicant', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->timestamps();
+			$table->string('sso', 20);
+			$table->string('LastName', 20);
+			$table->string('Firstname', 20);
+			$table->integer('Phone');
+			$table->string('Email', 40);
+			$table->date('GradDate');
+			$table->boolean('Graduate');
+			$table->integer('SpeakScore');
+			$table->primary('SSO');
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('applicant');
+	}
+
+}
