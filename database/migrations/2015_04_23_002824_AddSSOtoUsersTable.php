aa69af1d231971c3be70903b4100e2be95e892b3
jn9qc <jn9qc@mail.missouri.edu>
2015-04-22 19:49:23 -0500

    worked on registration and users

---
+++ database/migrations/2015_04_23_002824_AddSSOtoUsersTable.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSSOtoUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+            $table->string('sso', 20)->default('aaaaa');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+            $table->dropColumn('sso');
+		});
+	}
+
+}
