ae3a0d89ee7ee404cde54c8647f07ba6bb64da03
jn9qc <jn9qc@mail.missouri.edu>
2015-04-25 13:18:34 -0500

    fixing errors

--- database/migrations/2015_04_18_210017_create_applicant_table.php
+++ database/migrations/2015_04_18_210017_create_applicant_table.php
@@ -16,18 +16,14 @@ class CreateApplicantTable extends Migration {
 		{
 			$table->string('sso', 20)->unique();
 			$table->string('name', 40);
-			$table->string('phone', 10);
+			$table->string('phone', 20);
 			$table->string('email', 40);
-			$table->string('gpa', 4);
+			$table->string('gpa', 4)->nullable();
 			$table->string('graddate', 20);
 			$table->string('program', 20);
 			$table->string('previouswork', 140)->nullable();
-			$table->enum('previousteach', array(40))->nullable();
-			$table->enum('currentteach', array(4))->nullable();
-			$table->enum('futureteach', array(4))->nullable();
 			$table->integer('speakscore')->nullable();
 			$table->string('speakdate')->nullable();
-			$table->string('passwordtemp');
 			//$table->increments('id');
 			//$table->timestamps();
 			//$table->primary('sso');
