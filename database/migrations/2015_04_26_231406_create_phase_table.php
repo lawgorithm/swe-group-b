2dbd774178c48260d45f24f0db8e4a12d7897239
lawgorithm <lawrencewilliams.lw@gmail.com>
2015-04-26 23:22:42 +0000

    created migration for phase table, i.e. temporal modes

---
+++ database/migrations/2015_04_26_231406_create_phase_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhaseTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('phase', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->timestamps();
+			$table->timestamp('open');
+			$table->timestamp('transition');
+			$table->timestamp('close');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('phase');
+	}
+
+}
