ff895041623c28627ac365b756a27f324d7cb283
jn9qc <jn9qc@mail.missouri.edu>
2015-04-27 00:23:43 +0000

    added middleware stuff to chech roles

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -26,6 +26,7 @@ class FeedbackController extends Controller {
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('role');
     }
 
     /**
--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use App\Role;
 
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
 
@@ -30,5 +31,45 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	 * @var array
 	 */
 	protected $hidden = ['password', 'remember_token'];
+
+    /**
+     * @return mixed
+     */
+    public function getRole() {
+        $role = Role::where('sso', '=', $this->sso)->firstOrFail();
+        return $role->user_role;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isApplicant() {
+        if ($this->getRole() == "applicant") {
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * @return bool
+     */
+    public function isInstructor() {
+        if ($this->getRole() == "instructor") {
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * @return bool
+     */
+    public function isAdmin() {
+        if ($this->getRole() == "admin") {
+            return true;
+        }
+        return false;
+    }
 
 }
