1d04fa1b414b9b02c21f4791fe85abed8389db1b
jn9qc <jn9qc@mail.missouri.edu>
2015-04-30 00:26:54 -0500

    added hover over to display info in rank page

--- app/Http/Controllers/RankController.php
+++ app/Http/Controllers/RankController.php
@@ -3,6 +3,7 @@
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Course;
+use App\Applicant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
@@ -31,13 +32,8 @@ class RankController extends Controller {
         $courses = Course::all()->toArray();
 
         $db = new DB();
-        $applied = $db::table('users')
-                    ->join('applicantcourse', 'users.sso','=', 'applicantcourse.sso')
-                    ->where('applicantcourse.action', '=', '001')
-                    ->select('*')
-                    ->get();
 
-        return view('rank', ['courses' => $courses, 'applied' => $applied]);
+        return view('rank', ['courses' => $courses]);
 	}
 
     public function show($id){
@@ -47,9 +43,11 @@ class RankController extends Controller {
         $db = new DB();
         $applied = $db::table('users')
             ->join('applicantcourse', 'users.sso','=', 'applicantcourse.sso')
+            ->join('applicant', 'users.sso', '=', 'applicant.sso')
             ->where('applicantcourse.action', '=', '001')
             ->where('applicantcourse.courseid', '=', $id)
             ->select('*')
+            ->orderBy('applicantcourse.rank', 'asc')
             ->get();
 
         return view('rank', ['courses' => $courses, 'applied' => $applied]);
--- resources/views/rank.blade.php
+++ resources/views/rank.blade.php
@@ -1,9 +1,5 @@
 @extends('app')
 
-@section('headers')
-    <link href="/css/rank.css" rel="stylesheet" xmlns="http://www.w3.org/1999/html"/>
-@endsection
-
 @section('content')
 
     <div id="course_list">
@@ -13,20 +9,42 @@
             @foreach($courses as $course)
                 <a href="{{action('RankController@show', $course['courseid'])}} "><li class="single_course"><strong>{{$course['coursename']}}</strong></li></a>
             @endforeach
-
         </ul>
     </div>
     <div id="course_ranking">
-        <h3 class="course_header">Applicant Ranking</h3>
+        @if (isset($applied))
+            <h3 class="course_header">Applicant Ranking</h3>
 
-        <ul id="sortable">
+            <ul id="sortable">
+                @foreach($applied as $apply)
+                    <li class="ui-state-default" value="{{$apply->rank}}|{{$apply->sso}}"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>{{$apply->name}}</li>
+                @endforeach
+            </ul>
+            </br>
+            <button id="course_button">Submit</button>
+            <p id="submit_space"></p>
             @foreach($applied as $apply)
-                <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>{{$apply->name}}</li>
+                <div class="hidden" id="info-{{$apply->sso}}" hidden>
+                    <p>Email: {{$apply->email}}</p>
+                    <p>Phone: {{$apply->phone}}</p>
+                    <p>GPA: {{$apply->gpa}}</p>
+                    <p>Graddate: {{$apply->graddate}}</p>
+                    <p>Program: {{$apply->program}}</p>
+                    <p>Experience: {{$apply->previouswork}}</p>
+                    <p>Rank: {{$apply->rank}}</p>
+                    <p>Feedback: {{$apply->feedback}}</p>
+                </div>
             @endforeach
-        </ul>
-        </br>
-        <button id="course_button">Submit</button>
-        <p id="submit_space"></p>
+        @else
+            <h3 class="course_header">Courses Still Need Ranking</h3>
+            <ul id="list_course">
+                @foreach($courses as $course)
+                    <a href="{{action('RankController@show', $course['courseid'])}} "><li class="single_course"><strong>{{$course['coursename']}}</strong></li></a>
+                @endforeach
+            </ul>
+            </br>
+        @endif
+
     </div>
 
 @endsection
@@ -36,5 +54,6 @@
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/themes/flick/jquery-ui.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js"></script>
     <script src="/js/rank.js"></script>
+    <link href="/css/rank.css" rel="stylesheet" xmlns="http://www.w3.org/1999/html"/>
 
 @endsection
