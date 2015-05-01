d4ea76fdf46003e58b285140ecdcff41691f8726
jn9qc <jn9qc@mail.missouri.edu>
2015-05-01 15:05:06 -0500

    fixed error with exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -36,8 +36,9 @@ class Handler extends ExceptionHandler {
 	 */
 	public function render($request, Exception $e)
 	{
-        if ($e->getStatusCode() == 404)
-            return redirect('applicant/home');
+        if ($this->isHttpException($e))
+            if ($e->getStatusCode() == 404)
+                return redirect('applicant/home');
         return parent::render($request, $e);
 	}
 
