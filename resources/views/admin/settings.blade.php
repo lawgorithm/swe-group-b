f6992ac7cf9ee49b67779e25d4a6e2a16ca4da4c
lawgorithm <lawrencewilliams.lw@gmail.com>
2015-05-03 22:20:34 +0000

    made dates smaller on admin settings

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -1,53 +1,54 @@
 @extends('admin/admin')
 @section('headers')
-<style>
-.natural-dates {
-	font-size: 24px;
-	margin: 20px;
-}
-div {
-	font-size: 18px;
-}
-.natural-dates p {
-	margin: 15px;
-}
+    <style>
+        .natural-dates {
+            font-size: 20px;
+            margin: 20px;
+            padding: 5px;
+        }
+        div {
+            font-size: 18px;
+        }
+        .natural-dates p {
+            margin: 10px;
+        }
 
-p.phase-statement {
-	text-align: center;
-}
+        p.phase-statement {
+            text-align: center;
+        }
 
-div.alert {
-	margin-bottom: 45px;
-}
-.settings-form {
-	text-align: center;
-}
-</style>
+        div.alert {
+            margin-bottom: 45px;
+        }
+        .settings-form {
+            text-align: center;
+        }
+    </style>
 @endsection
 @section('content')
-<div class="container">
-    <p class="phase-statement">We are currently in Phase {{$phaseCode}}: {{$phaseDefinition}}</p>
-	@if ($phaseCode !== 0)
-	<div class="alert alert-info text-center natural-dates" role="alert">
+    <div class="container">
+        <p class="phase-statement">We are currently in Phase {{$phaseCode}}: {{$phaseDefinition}}</p>
+        @if ($phaseCode !== 0)
+            <div class="alert alert-info text-center natural-dates" role="alert">
 
-		<p>We will <strong>begin collecting applications</strong> on <u>{{$open}}</u>.</p>
-		<p><u>{{$transition}}</u> is the <strong>deadline for student submissions</strong>.</p>
-		<p><u>{{$close}}</u> is the <strong>deadline for instructor feedback</strong>.</p>
-		<p><small>Set By: {{$author}}</small></p>
-	</div>
-	@else
-	<div class="alert alert-warning text-center" role="alert">
-		<p>Your application phases have not been set.</p>
-		
-	</div>
-	@endif
-	<div class="settings-form">
-		{!! BootForm::open() !!}
-		{!! BootForm::date('Begin Collecting Applications', 'open') !!}
-		{!! BootForm::date('Application Submission Deadline', 'transition') !!}
-		{!! BootForm::date('Instructor Feedback Deadline', 'close') !!}
-		{!! BootForm::submit('Submit') !!}
-		{!! BootForm::close() !!}
-	</div>
-</div>
+                <p>We will <strong>begin collecting applications</strong> on <u>{{$open}}</u>.</p>
+                <p><u>{{$transition}}</u> is the <strong>deadline for student submissions</strong>.</p>
+                <p><u>{{$close}}</u> is the <strong>deadline for instructor feedback</strong>.</p>
+                <p><small>Set By: {{$author}}</small></p>
+            </div>
+        @else
+            <div class="alert alert-warning text-center" role="alert">
+                <p>Your application phases have not been set.</p>
+
+            </div>
+        @endif
+        <div class="settings-form">
+            {!! BootForm::open() !!}
+            {!! BootForm::date('Begin Collecting Applications', 'open') !!}
+            {!! BootForm::date('Application Submission Deadline', 'transition') !!}
+            {!! BootForm::date('Instructor Feedback Deadline', 'close') !!}
+            {!! BootForm::submit('Submit') !!}
+            {!! BootForm::close() !!}
+        </div>
+    </div>
 @endsection
