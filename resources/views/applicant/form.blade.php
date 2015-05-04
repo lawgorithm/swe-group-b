9ebde5c504078d3d8ced0fb7ef445f4c23b6db42
random9s <jake.august.parham@gmaiil.com>
2015-05-04 19:21:46 +0000

    Added konami code, enter the konami code on applicant home or apply pages and see what happens

--- resources/views/applicant/form.blade.php
+++ resources/views/applicant/form.blade.php
@@ -137,5 +137,6 @@
 @section('scripts')
 <script src="/js/formatter.js"></script>
 <script src="/js/form.js"></script>
+<script src="/js/konami.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 @endsection
