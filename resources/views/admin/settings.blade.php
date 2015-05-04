f0b9b871bade468b6af92ab17059beb08bfb2646
lawgorithm <lawrencewilliams.lw@gmail.com>
2015-05-04 18:29:28 -0500

    added restrictions to admin/setting date inputs

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -58,3 +58,78 @@
     <br>
     <br>
 @endsection
+
+@section('scripts')
+    <script>
+
+        var s = now();
+
+        var open = document.getElementById("open");
+        var trans = document.getElementById("transition");
+        var close = document.getElementById("close");
+
+
+        open.setAttribute("onInput", "enableTransition()");
+        transition.setAttribute("onInput", "enableClose()");
+
+        trans.setAttribute("disabled", "disabled");
+        close.setAttribute("disabled", "disabled");
+
+        open.setAttribute("min", s["min"]);
+
+        open.setAttribute("max", s["max"]);
+        trans.setAttribute("max", s["max"]);
+        close.setAttribute("max", s["max"]);
+
+        open.setAttribute("required", "required");
+        trans.setAttribute("required", "required");
+        close.setAttribute("required", "required");
+
+        function enableTransition() {
+            var open = document.getElementById("open");
+            var trans = document.getElementById("transition");
+
+            trans.removeAttribute("disabled");
+            trans.setAttribute("min", open.value);
+        }
+
+        function enableClose(){
+            var trans = document.getElementById("transition");
+            var close = document.getElementById("close");
+
+            close.removeAttribute("disabled");
+            close.setAttribute("min", trans.value);
+        }
+
+
+        function now() {
+            var date = new Date();
+            var y = date.getFullYear();
+            var m = date.getMonth() + 1;
+            var d = date.getDate();
+
+            var s = "";
+
+            s += y;
+            s += "-";
+            s += m < 10 ? "0" + m : m;
+            s += "-";
+            s += d < 10 ? "0" + d : d;
+
+            var temp = s.split("-");
+            var temp2 = 0 + + temp[0];
+            temp2 += 1;
+            temp[0] = temp2;
+            temp.join("-");
+
+            hold = [];
+            hold["min"] = s;
+            hold["max"] = temp.join("-");
+
+            return hold;
+        }
+
+    </script>
+
+
+@endsection
